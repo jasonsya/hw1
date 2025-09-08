81adfce9d0ea5cc48d50e13e9b2fdee9e3e51bce
Jason Yang <jasonsya@usc.edu>
2025-09-08 00:25:18 +0000

    all done with part 1

--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,41 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
+using namespace std;
+
+void printList(Node* head) {
+  while (head != nullptr) {
+    cout << head->value << " ";
+    head = head->next;
+  }
+  cout << endl;
+}
+
+void deleteList(Node* head) {
+  while (head != nullptr) {
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+  }
+}
 
 int main(int argc, char* argv[])
 {
+  Node* head = new Node(1, nullptr);
+  head->next = new Node(2, nullptr);
+  head->next->next = new Node(3, nullptr);
 
+  Node* odd = nullptr;
+  Node* even = nullptr;
+
+  split(head, odd, even);
+
+  cout << "Odds: ";
+  printList(odd);
+  cout << "Evens: ";
+  printList(even);
+
+  deleteList(odd);
+  deleteList(even);
 }
